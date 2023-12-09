795454300fd0b6f745681e2b1f229257314c70bf
cattuongtu <cattuongtu3@gmail.com>
2023-12-08 17:24:24 -0800

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Cat-Tuong Tu on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
